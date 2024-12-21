300d6cc5a1f5b49461313173537bca03354b0c43
AI-AMAD <lkdfj2212@gmail.com>
2024-12-21 18:45:51 +0900

    메인 페이지 컴포넌트 작업

---
+++ frontend/src/components/TheSidebar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="d-flex">
+    <!-- Sidebar -->
+    <nav class="bg-dark text-white p-3 vh-100" style="width: 10%; border-radius: 0px 15px 15px 0px">
+      <h3 class="mb-4">Auto - Trip!!</h3>
+      <ul class="nav flex-column">
+        <li class="nav-item mb-3">
+          <router-link class="nav-link text-white fs-5" active-class="active" to="/main/setting"
+            >내 설정</router-link
+          >
+        </li>
+        <li class="nav-item mb-3">
+          <router-link class="nav-link text-white fs-5" active-class="active" to="/main/plan"
+            >여행 계획</router-link
+          >
+        </li>
+        <li class="nav-item mb-3">
+          <router-link class="nav-link text-white fs-5" active-class="active" to="/main/schedule"
+            >일정 보기</router-link
+          >
+        </li>
+        <li class="nav-item mb-3">
+          <router-link class="nav-link text-white fs-5" active-class="active" to="/main/diary"
+            >다이어리</router-link
+          >
+        </li>
+      </ul>
+    </nav>
+
+    <!-- Content -->
+    <div class="flex-grow-1 p-4">
+      <router-view></router-view>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,5 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '@/views/LoginView.vue'
+import MainPage from '@/views/MainPage.vue'
+import MySetting from '@/components/MySetting.vue'
+import MyPlan from '@/components/MyPlan.vue'
+import MySchedule from '@/components/MySchedule.vue'
+import MyDiary from '@/components/MyDiary.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +14,29 @@ const router = createRouter({
       name: 'login',
       component: LoginView
     },
+    {
+      path: '/main',
+      name: 'mainpage',
+      component: MainPage,
+      children: [
+        {
+          path: 'setting',
+          component: MySetting
+        },
+        {
+          path: 'plan',
+          component: MyPlan
+        },
+        {
+          path: 'schedule',
+          component: MySchedule
+        },
+        {
+          path: 'diary',
+          component: MyDiary
+        }
+      ]
+    }
   ]
 })
 
