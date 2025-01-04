cf947ff3df249ade556866a681184cb5701a08a7
AI-AMAD <lkdfj2212@gmail.com>
2025-01-04 18:50:19 +0900

    where UI 작업

--- frontend/src/components/WhenTrip.vue
+++ frontend/src/components/WhenTrip.vue
@@ -1,15 +1,21 @@
 <template>
-  <div>
-    <div class="card">
-      <h5 class="card-header">언제 여행을 떠나시나요?</h5>
-      <div class="card-body">
-        <h5 class="card-title">Special title treatment</h5>
-        <p class="card-text">
-          With supporting text below as a natural lead-in to additional content.
-        </p>
-        <a href="#" class="btn btn-primary">Go somewhere</a>
+  <div class="row">
+    <div class="col-8">
+      <!-- 달력 -->
+      <div class="card">
+        <h5 class="card-header">언제 여행을 떠나시나요?</h5>
+        <div class="card-body">
+          <h5 class="card-title">Special title treatment</h5>
+          <p class="card-text">
+            With supporting text below as a natural lead-in to additional content.
+          </p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
       </div>
+      <!-- end of 달력 -->
     </div>
+
+    <div class="col-4"></div>
   </div>
 </template>
 
