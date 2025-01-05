dd3ac17f3ba367af6eb786b5f1978fed2749026f
AI-AMAD <lkdfj2212@gmail.com>
2025-01-05 16:24:58 +0900

    깃 캐시 삭제

---
+++ frontend/src/components/WhereTrip.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="row">
+    <div class="col-8">
+      <div class="card">
+        <h5 class="card-header">어디로 여행을 떠나시나요?</h5>
+        <div class="card-body">
+          <h5 class="card-title">Special title treatment</h5>
+          <p class="card-text">
+            With supporting text below as a natural lead-in to additional content.
+          </p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+    <div class="col-4">
+      <form class="row" @submit.prevent="search">
+        <div class="col-auto">
+          <input
+            v-model="inputLocation"
+            type="text"
+            class="form-control"
+            placeholder="여행 장소를 입력 하세요"
+          />
+        </div>
+        <div class="col-auto">
+          <button type="submit" class="btn btn-info mb-3">검색</button>
+        </div>
+      </form>
+      <div class="row">
+        <div class="col">
+          <div class="card">
+            <h5 class="card-header">이 장소가 맞나요?</h5>
+            <div class="card-body">
+              <p class="card-text">{{ searchedLocation }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row mt-2">
+        <div class="col">
+          <SaveButton @save="saveData"></SaveButton>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import SaveButton from '@/components/SaveButton.vue'
+import { ref } from 'vue'
+
+const inputLocation = ref('')
+const searchedLocation = ref('')
+const search = () => {
+  searchedLocation.value = inputLocation.value
+}
+
+const saveData = () => {
+  alert(`해당 장소를 저장합니다: ${searchedLocation.value}`)
+}
+</script>
+
+<style scoped></style>
---
+++ frontend/src/views/MainPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <TheSidebar></TheSidebar>
+  </div>
+</template>
+
+<script setup>
+import TheSidebar from '@/components/TheSidebar.vue'
+</script>
+
+<style lang="scss" scoped></style>
