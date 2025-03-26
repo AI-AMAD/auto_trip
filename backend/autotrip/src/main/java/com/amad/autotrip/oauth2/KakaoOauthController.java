aa71c564bd21010b9655043e4bd09f6c40e6a374
AI-AMAD <lkdfj2212@gmail.com>
2025-03-26 09:21:05 +0900

    oauth login for kakao - 토큰정보 프론트로 리다이렉트

--- backend/autotrip/src/main/java/com/amad/autotrip/oauth2/KakaoOauthController.java
+++ backend/autotrip/src/main/java/com/amad/autotrip/oauth2/KakaoOauthController.java
@@ -1,41 +1,70 @@
 package com.amad.autotrip.oauth2;
 
 import com.amad.autotrip.dto.KakaoUserInfoResponseDto;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
+import com.amad.autotrip.dto.LoginDto;
+import com.amad.autotrip.dto.TokenDto;
+import com.amad.autotrip.dto.UserDto;
+import com.amad.autotrip.repository.UserRepository;
+import com.amad.autotrip.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
+
+import java.io.IOException;
+
 
 @RestController
 @RequestMapping("/api")
 public class KakaoOauthController {
 
     private final KakaoOauthService kakaoOauthService;
+    private final UserRepository userRepository;
+    private final UserService userService;
 
-    public KakaoOauthController(KakaoOauthService kakaoOauthService) {
+    public KakaoOauthController(KakaoOauthService kakaoOauthService, UserRepository userRepository, UserService userService) {
         this.kakaoOauthService = kakaoOauthService;
+        this.userRepository = userRepository;
+        this.userService = userService;
     }
 
     @GetMapping("/auth/kakao/callback")
-    public ResponseEntity<?> callback(@RequestParam("code") String code) {
+    public void callback(@RequestParam("code") String code, HttpServletResponse response) throws IOException {
         System.out.println("code: " + code);
         System.out.println("컨트롤러 진입은 한거니??");
         String accessToken = kakaoOauthService.getAccessTokenFromKakao(code);
 
         KakaoUserInfoResponseDto userInfo = kakaoOauthService.getUserInfo(accessToken);
 
-        System.out.println("userinfo--->: " + userInfo);
+        UserDto user = new UserDto();
+        user.setUsername(String.valueOf(userInfo.getId()));
+        user.setPassword("kakao");
+        user.setNickname(userInfo.getKakaoAccount().getProfile().getNickName());
+
+        ResponseEntity<TokenDto> token = null;
+
+        if (userRepository.findOneWithAuthoritiesByUsername(user.getUsername()).orElse(null) != null) {
+            LoginDto loginDto = new LoginDto();
+            loginDto.setUsername(String.valueOf(userInfo.getId()));
+            loginDto.setPassword("kakao");
+
+            token = userService.login(loginDto);
+        } else {
+            userService.signup(user);
+            LoginDto loginDto = new LoginDto();
+            loginDto.setUsername(String.valueOf(userInfo.getId()));
+            loginDto.setPassword("kakao");
+
+            token = userService.login(loginDto);
+        }
+
+        String redirectUrl = "http://localhost:5173/?token=" + token;
+        response.sendRedirect(redirectUrl);
+
         // 여기에 서버 사용자 로그인(인증) 또는 회원가입 로직 추가
-        return new ResponseEntity<>(HttpStatus.OK);
+//        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 }
--- backend/autotrip/src/main/java/com/amad/autotrip/service/UserService.java
+++ backend/autotrip/src/main/java/com/amad/autotrip/service/UserService.java
@@ -2,29 +2,52 @@ package com.amad.autotrip.service;
 
 import java.util.Collections;
 
+import com.amad.autotrip.dto.LoginDto;
+import com.amad.autotrip.dto.TokenDto;
 import com.amad.autotrip.dto.UserDto;
 import com.amad.autotrip.dto.UserProfileDto;
 import com.amad.autotrip.entity.Authority;
 import com.amad.autotrip.entity.Users;
 import com.amad.autotrip.exception.DuplicateMemberException;
 import com.amad.autotrip.exception.NotFoundMemberException;
+import com.amad.autotrip.jwt.JwtFilter;
+import com.amad.autotrip.jwt.TokenProvider;
 import com.amad.autotrip.repository.AuthorityRepository;
 import com.amad.autotrip.repository.UserRepository;
 import com.amad.autotrip.util.SecurityUtil;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class UserService {
+
     private final UserRepository userRepository;
     private final AuthorityRepository authorityRepository;
     private final PasswordEncoder passwordEncoder;
+    private final TokenProvider tokenProvider;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
-    public UserService(UserRepository userRepository, AuthorityRepository authorityRepository, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository,
+                       AuthorityRepository authorityRepository,
+                       PasswordEncoder passwordEncoder,
+                       TokenProvider tokenProvider,
+                       AuthenticationManagerBuilder authenticationManagerBuilder) {
+
         this.userRepository = userRepository;
         this.authorityRepository = authorityRepository;
         this.passwordEncoder = passwordEncoder;
+        this.tokenProvider = tokenProvider;
+        this.authenticationManagerBuilder = authenticationManagerBuilder;
     }
 
     @Transactional
@@ -65,5 +88,21 @@ public class UserService {
                         .flatMap(userRepository::findOneWithAuthoritiesByUsername)
                         .orElseThrow(() -> new NotFoundMemberException("Member not found"))
         );
+    }
+
+    public ResponseEntity<TokenDto> login(@Valid @RequestBody LoginDto loginDto) {
+
+        UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(loginDto.getUsername(), loginDto.getPassword());
+
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = tokenProvider.createToken(authentication);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer " + jwt);
+
+        return new ResponseEntity<>(new TokenDto(jwt), httpHeaders, HttpStatus.OK);
     }
 }
