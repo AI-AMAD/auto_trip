f2f424ecb4eb43e32d7d39211600736f45571d38
AI-AMAD <lkdfj2212@gmail.com>
2025-05-08 09:31:29 +0900

    setting 값에 따른 여행 장소 검색

--- backend/autotrip/src/main/java/com/amad/autotrip/controller/PlanController.java
+++ backend/autotrip/src/main/java/com/amad/autotrip/controller/PlanController.java
@@ -5,6 +5,9 @@ import com.amad.autotrip.service.PlanService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class PlanController {
@@ -27,10 +30,39 @@ public class PlanController {
     }
 
     @PostMapping("/auto/plan")
-    public ResponseEntity<?> autoPlan(@RequestBody TripSummaryDto TripSummaryDto) {
+    public ResponseEntity<?> autoPlan(@RequestBody TripSummaryDto tripSummaryDto) {
         try {
             // userSetting 정보 배열에 담기
+            List<String> settings = new ArrayList<>();
 
+            String username = tripSummaryDto.getUsername();
+            String place = tripSummaryDto.getPlace();
+
+            // Boolean 값을 체크하여 true인 경우 리스트에 추가
+            if (tripSummaryDto.isActivity()) {
+                settings.add("레포츠");
+            }
+            if (tripSummaryDto.isMuseum()) {
+                settings.add("박물관");
+            }
+            if (tripSummaryDto.isCafe()) {
+                settings.add("카페");
+            }
+            if (tripSummaryDto.isTourAtt()) {
+                settings.add("관광 명소");
+            }
+
+            // setting에 맞는 여행 장소 검색
+//            planService.naverSearch(place, settings);
+
+            // 검색한 여행 장소 진위 확인
+
+            // 여행 장소 이미지 검색
+
+            // DB 저장
+
+
+            return ResponseEntity.ok("여행 계획 수립 완료!");
 
         } catch (Exception e) {
             return ResponseEntity.badRequest().body(new ErrorDto(500, "서버 오류: " + e.getMessage()));
