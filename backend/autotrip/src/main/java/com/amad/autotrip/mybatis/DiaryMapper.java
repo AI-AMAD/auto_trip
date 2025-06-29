2e0ce5a9c06cbc2083bb5163e54d8ed03eea17f3
AI-AMAD <lkdfj2212@gmail.com>
2025-06-30 08:48:39 +0900

    accommodation 테이블 생성 및 컨트롤러, 서비스, 매퍼 생성

---
+++ backend/autotrip/src/main/java/com/amad/autotrip/mybatis/DiaryMapper.java
@@ -0,0 +1,7 @@
+package com.amad.autotrip.mybatis;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface DiaryMapper {
+}
---
+++ backend/autotrip/src/main/java/com/amad/autotrip/service/DiaryService.java
@@ -0,0 +1,7 @@
+package com.amad.autotrip.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class DiaryService {
+}
